4568abb8ffa7f093d2718f48cdeadccf48ffc7d7
huangli <huangli@leedarson.com>
2025-01-02 18:59:51 +0800

    telilnk demo 2 test

--- telink_zigbee_ble_concurrent_sdk/build/iot_riscv_b92/bootloader_b92/platform/boot/link_cfg.d
+++
@@ -1,6 +0,0 @@
-platform/boot/link_cfg.o: \
- D:/hl/telink/zigbee_test/telink_zigbee_ble_concurrent_sdk_20241217/platform/boot/link_cfg.S \
- ../../../apps/bootLoader/version_cfg.h \
- ../../../apps/bootLoader/../common/comm_cfg.h
-../../../apps/bootLoader/version_cfg.h:
-../../../apps/bootLoader/../common/comm_cfg.h:
--- telink_zigbee_ble_concurrent_sdk/build/iot_riscv_b92/bootloader_b92/proj/common/mempool.d
+++
@@ -1,8 +0,0 @@
-proj/common/mempool.o: \
- D:/hl/telink/zigbee_test/telink_zigbee_ble_concurrent_sdk_20241217/proj/common/mempool.c \
- D:/hl/telink/zigbee_test/telink_zigbee_ble_concurrent_sdk_20241217/proj/common/mempool.h \
- D:/hl/telink/zigbee_test/telink_zigbee_ble_concurrent_sdk_20241217/proj/common/utility.h \
- D:/hl/telink/zigbee_test/telink_zigbee_ble_concurrent_sdk_20241217/proj/common/types.h
-D:/hl/telink/zigbee_test/telink_zigbee_ble_concurrent_sdk_20241217/proj/common/mempool.h:
-D:/hl/telink/zigbee_test/telink_zigbee_ble_concurrent_sdk_20241217/proj/common/utility.h:
-D:/hl/telink/zigbee_test/telink_zigbee_ble_concurrent_sdk_20241217/proj/common/types.h:
